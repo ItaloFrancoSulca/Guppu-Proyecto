a420d24c7cbd9dce98cfc7fd5cd199db1aca5434
JoseAlcas <jose.ach.22@outlook.com>
2018-06-15 10:56:10 -0500

    Se añadio script de Modelo - SFX

---
+++ Assets/1 - Scripts/1.4 - Music&SFX/SFX Controler/SoundEffect_Modelo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect_Modelo : MonoBehaviour {
+    public AudioClip sfxNarrador;
+    
+    public AudioClip sfxRocoton;
+    
+    public AudioClip sfxWin;
+    
+    public AudioClip sfxFall;
+    
+    public AudioClip sfxLose;
+    
+    public AudioClip sfxInteresante;
+    
+    public float volume;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
