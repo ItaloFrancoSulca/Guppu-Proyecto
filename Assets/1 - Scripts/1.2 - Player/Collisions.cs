d45490609578ee2015f2d55a4c75daeb3b90f0a6
ItaloFrancoSulca <italo.franco.16@hotmail.com>
2018-06-15 11:43:46 -0500

    Added Vista Script

--- Assets/1 - Scripts/1.2 - Player/Collisions.cs
+++ Assets/1 - Scripts/1.2 - Player/Collisions.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 public class Collisions : MonoBehaviour {
 
     public bool dead;
-
+    public GameObject soundEffectController;
+    SoundEffect_Vista soundEffect_Vista;
 	// Use this for initialization
 	void Start () {
-		
+		soundEffect_Vista = soundEffectController.GetComponent<SoundEffect_Vista>();
 	}
 	
 	// Update is called once per frame
@@ -23,6 +24,33 @@ public class Collisions : MonoBehaviour {
         {
             Time.timeScale = 0;
             dead = true;
+            
+            int playLoseSound = Random.Range(1,3);
+
+            switch(playLoseSound){
+                case 1:
+                soundEffect_Vista.RocotonSFX();
+                break;
+
+                case 2:
+                soundEffect_Vista.LoseSFX();
+                break;
+
+            }
+            
+        }
+
+        
+    }
+
+    private void OnTriggerEnter (Collider obj){
+
+        if (obj.gameObject.tag == "Nether"){
+            soundEffect_Vista.FallSFX();
+        }
+
+        if (obj.gameObject.tag == "Rune"){
+            soundEffect_Vista.InteresanteSFX();
         }
     }
 }
---
+++ Assets/1 - Scripts/1.4 - Music&SFX/SFX Controler/SoundEffect_Vista.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect_Vista : MonoBehaviour {
+    AudioSource audioSource;
+    SoundEffect_Modelo soundEffect_Modelo;
+    // Use this for initialization
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        soundEffect_Modelo = GetComponent<SoundEffect_Modelo>();
+    }
+
+    void Start()
+    {
+        audioSource.PlayOneShot(soundEffect_Modelo.sfxNarrador, soundEffect_Modelo.volume);
+    }
+
+    // Update is called once per frame
+    public void RocotonSFX()
+    {
+        audioSource.PlayOneShot(soundEffect_Modelo.sfxRocoton, soundEffect_Modelo.volume);
+    }
+    public void WinSFX()
+    {
+        audioSource.PlayOneShot(soundEffect_Modelo.sfxWin, soundEffect_Modelo.volume);
+    }
+    public void FallSFX()
+    {
+        audioSource.PlayOneShot(soundEffect_Modelo.sfxFall, soundEffect_Modelo.volume);
+    }
+    public void LoseSFX()
+    {
+        audioSource.PlayOneShot(soundEffect_Modelo.sfxLose, soundEffect_Modelo.volume);
+    }
+    public void InteresanteSFX()
+    {
+        audioSource.PlayOneShot(soundEffect_Modelo.sfxInteresante, soundEffect_Modelo.volume);
+    }
+}
